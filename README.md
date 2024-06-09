e68aec2f5c39a2b2c04074dc1813696d0e5736dd
AEntropia <sandman2871@gmail.com>
2024-06-09 18:33:59 -0300

    vercel

---
+++ README.md
@@ -0,0 +1,45 @@
+![Foto de Fundo para Linkedin Amarelo Lâmpada Minimalista (1)](https://github.com/Ecosrev/ecosrevMongo/assets/126609101/5f7ca8a9-9032-4ae4-9ca8-96eb2b656ffb)
+
+🍃Grupo Ecosrev
+---
+
+👤Integrantes
+
+- Gabriel Yamaoka Bernardes
+- Mariana Aiko
+- Mateus Ferreira
+
+###
+>> Trabalho desenvolvido com o foco no projeto Ecosrev, visando uma melhora em sua base tanto no front-end quanto no back-end.
+
+>> O Ecosrev é um projeto focado em melhorar o meio ambiente, fornecendo um incentivo ao recolher lixo eletrônico dos habitantes em troca de benefícios
+###
+## 📃Sobre o projeto
+
+O Projeto EcosRev é um website desenvolvido com o propósito de incentivar o descarte correto de resíduos eletroeletrônicos, promovendo a conexão entre empresas coletoras de lixo eletrônico e cidadãos. O objetivo principal é facilitar o processo de descarte, aumentando a conscientização ambiental e contribuindo para a preservação do meio ambiente.
+
+
+## 💡Protótipo 
+
+Acesse o link de visualização interativo do nosso Figma: https://www.figma.com/design/szHO2FNtWtuBxW8hrbRYau/ECOSREV-BD?node-id=0-1&t=OjwOPtuo1edvRLnb-1
+
+## 🔄Backlogs 
+Confira o nosso backlog no link abaixo. 
+
+Jira: https://ecosrev.atlassian.net/jira/software/projects/SCRUM/boards/1/backlog?epics=visible&issueParent=10016&atlOrigin=eyJpIjoiNDdlNzFlNmU3MTY1NGIwODhhMTgwNjIyZDc0MTEzNTAiLCJwIjoiaiJ9
+
+## 💬User Stories
+![USERSTORY-imagens-0](https://github.com/Ecosrev/ecosrevMongo/assets/126609101/f9f8a74d-2583-4b7c-8928-74107bd9466e)
+![USERSTORY-imagens-1](https://github.com/Ecosrev/ecosrevMongo/assets/126609101/d7a17d64-5388-4352-8725-9cf7dcfcd84e)
+
+
+
+## 🌐Tecnologias Utilizadas
+
+- Frontend: HTML, CSS, JavaScript, React.js
+- Backend: Node.js, Express.js, MongoDB
+- APIs: Geolocalização (para encontrar locais de descarte próximos), Serviços de Mapas (para visualização dos locais de descarte)
+- Outras ferramentas e bibliotecas: Bootstrap, JWT para autenticação.
+
+## 📚Documentação Geral
+ Link para documentação: https://heyzine.com/flip-book/a741486d0b.html
---
+++ public/js/userPut.js
@@ -0,0 +1,110 @@
+const urlBase = window.location.href.replace(/\/[^\/]*$/, '') + '/api'
+const access_token = localStorage.getItem('token') || null
+const resultadoModal = new bootstrap.Modal(document.getElementById('modalMensagem'))
+const attForm = new bootstrap.Modal(document.getElementById('modalForm'))
+
+document.addEventListener('DOMContentLoaded', async () => {
+    tipo = await tipo()   
+    if(tipo != "Admin"){
+            alert('Acesso negado. Não é admin.');
+            window.location.href = '/index.html';
+        }
+});
+
+async function tipo(){
+    await fetch(`${urlBase}/usuario/pontos`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'access-token': access_token
+        }
+    })
+        .then(response => response.json())
+        .then(data => {
+            tipo = data[0].tipo
+        })
+        return tipo
+}
+
+async function carregaUsuario(){
+    const tabela = document.getElementById('dadosTabela')
+    tabela.innerHTML = '' //limpa antes de recarregar
+    //Faremos a requisição GET para a nossa API REST
+    await fetch(`${urlBase}/usuario`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'access-token' : access_token
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        //console.table(data)
+        data.forEach(usuario => {
+            tabela.innerHTML += `
+            <tr>
+              <td>${usuario.nome}</td>
+              <td>${usuario.email}</td>
+              <td>${usuario.pontos}</td>
+              <td>
+        <button class='btnAtualizar' onclick='carregaAtt("${usuario._id}")'>Atualizar </button>
+              </td>
+            </tr>
+            `
+        })
+    })
+}
+
+async function atualizaUsuario(usuario){
+    await fetch(`${urlBase}/usuario/pontosPut`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+            'access-token': access_token
+        },
+        body: JSON.stringify(usuario)
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.acknowledged) {
+            alert('Usuário atualizado com sucesso!')
+            //atualizamos a listagem
+            carregaUsuario()
+        } else if (data.errors){
+ const errorMessages = data.errors.map(error => error.msg).join('\n')
+ document.getElementById('mensagem').innerHTML = `<span class='text-danger'>${errorMessages}</span>`
+ resultadoModal.show() //Mostra o modal
+        }
+    })
+}
+
+document.getElementById('botaoAtt').addEventListener('click', function (event){
+    event.preventDefault() // evita o recarregamento
+    //let beneficio = {} // Objeto beneficio
+    usuario = {
+        "_id": document.getElementById('idAtt').value,
+        "nome": document.getElementById('nome1').value,
+        "pontos": document.getElementById('pontos4').value,
+    } /* fim do objeto */
+    //alert(JSON.stringify(beneficio)) //apenas para testes
+    atualizaUsuario(usuario)
+    attForm.hide()
+})
+
+async function carregaAtt(id){
+    attForm.show()
+    await fetch(`${urlBase}/usuario/id/${id}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'access-token' : access_token
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        //console.log(data[0]._id)
+        document.getElementById('idAtt').value = data[0]._id
+        document.getElementById('nome1').value = data[0].nome
+        document.getElementById('pontos4').value = data[0].pontos
+    })
+}
