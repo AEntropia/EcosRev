9f2d07410001ddc305d0688ccee387258e64e83d
AEntropia <sandman2871@gmail.com>
2024-06-09 18:52:14 -0300

    vercel again

--- swagger.js
+++ swagger.js
@@ -10,7 +10,7 @@ const doc = {
     },
     host: 'https://ecos-rev.vercel.app',// LINK DO VERCEL VAI AQUI
     basePath: "/api",
-    schemes: ['http'], //NO VERCEL DARA ERRO, TROCAR POR HTTPS AMEM
+    schemes: ['https'], //NO VERCEL DARA ERRO, TROCAR POR HTTPS AMEM
     consumes: ['application/json'],
     produces: ['application/json'],
     securityDefinitions: {
